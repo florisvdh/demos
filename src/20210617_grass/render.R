5b6d20aef5859d01d0e8813743c6747256530119
florisvdh <floris.vanderhaeghe@inbo.be>
2021-06-16 11:56:58 +0200

    20210617_grass: use xfun::Rscript_call() in render.R

--- src/20210617_grass/render.R
+++ src/20210617_grass/render.R
@@ -1,5 +1,8 @@
 library(rprojroot)
 gitroot <- find_root(is_git_root)
 setwd(file.path(gitroot, "src/20210617_grass"))
-bookdown::render_book(output_file= file.path(gitroot,
-                                             "docs/20210617_grass/index"))
+xfun::Rscript_call(
+    bookdown::render_book,
+    list(output_file = file.path(gitroot,
+                                 "docs/20210617_grass/index"))
+)
